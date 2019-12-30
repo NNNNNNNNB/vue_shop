7f70b389b001d02c5de276e5bf66f3c90a516083
chy <207976734@qq.com>
2019-12-30 16:00:24 +0800

    2019.12.30完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="home">
+        home页面
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home'
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
-import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+
+//字体图标
+import './assets/fonts/iconfont.css'
+
+//全局样式表
+import './assets/css/global.css'
+
+//配置axios网络请求
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
